b1be3e2a031019f5366e99fd4bd4e7c4aed62f07
Rakibul Ahasan <59710234+PaponAhasan@users.noreply.github.com>
2022-04-30 14:33:39 +0600

    Update Delete middle element of a stack.cpp

--- Fundamental/Recursion/Practice Problem/Delete middle element of a stack.cpp
+++ Fundamental/Recursion/Practice Problem/Delete middle element of a stack.cpp
@@ -19,34 +19,4 @@ class Solution
          midDelete(s,0,size);
     }
 };
-
-int main() {
-    int t;
-    cin>>t;
-    
-    while(t--)
-    {
-        int sizeOfStack;
-        cin>>sizeOfStack;
-        
-        stack<int> myStack;
-        
-        for(int i=0;i<sizeOfStack;i++)
-        {
-            int x;
-            cin>>x;
-            myStack.push(x);    
-        }
-
-            Solution ob;
-            ob.deleteMid(myStack,myStack.size());
-            while(!myStack.empty())
-            {
-                cout<<myStack.top()<<" ";
-                myStack.pop();
-            }
-        cout<<endl;
-    }   
-    return 0;
-}
 // } Driver Code Ends
