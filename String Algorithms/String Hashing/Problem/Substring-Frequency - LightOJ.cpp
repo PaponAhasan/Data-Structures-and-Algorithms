979d74cd9a9e2be4366029c2a1cd7e3affc20931
Rakibul Ahasan <59710234+PaponAhasan@users.noreply.github.com>
2022-08-09 16:12:41 +0600

    Create Substring-Frequency - LightOJ.cpp

---
+++ String Algorithms/String Hashing/Problem/Substring-Frequency - LightOJ.cpp
@@ -0,0 +1,53 @@
+https://lightoj.com/problem/substring-frequency
+
+#include <bits/stdc++.h>
+using namespace std;
+#define BASE 129
+#define MOD 1000000007
+#define MAX_S 1000005
+
+long long h[MAX_S + 5], pw[MAX_S + 5];
+void generatePrefixHash(string &s){
+    h[0] = s[0];
+    for(int i = 1; i < s.size(); i++){
+        h[i] = ((h[i - 1] * BASE) + s[i]) % MOD;
+    }
+    pw[0] = 1;
+    for(int i = 1;i <= s.size(); i++){
+        pw[i] = (pw[i - 1] * BASE) % MOD;
+    }
+}
+
+long long generateHash(string &s){
+    long long H = 0;
+    for(auto c : s){
+        H = ((H * BASE) + c) % MOD;
+    }
+    return H;
+}
+
+long long getHash(int L, int R){
+    if(L == 0) return h[R];
+    return (h[R] - (h[L - 1] * pw[R - L + 1] % MOD) + MOD) % MOD;
+}
+
+int main() {
+    int tc,Case = 0;
+    scanf("%d",&tc);
+    while(tc--){
+        printf("Case %d: ",++Case);
+        string s1,s2;
+        cin >> s1 >> s2;
+    	  generatePrefixHash(s1);
+        long long hashOfS2 = generateHash(s2);
+        long long cnt = 0;
+        for(int i = 0; i + s2.size() <= s1.size(); i++){
+            if(getHash(i, i + s2.size() - 1) == hashOfS2){
+                cnt++;
+            }
+        }
+        printf("%lld\n",cnt);
+    }
+    
+	return 0;
+}
